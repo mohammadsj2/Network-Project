c3949b8bb2a17602fbfda8757e5939691212f35d
mohammadsj2 <mohammad.sj2@gmail.com>
2022-02-04 05:30:15 +0330

    Add UDPTunnel

--- tunnel.py
+++ tunnel.py
@@ -1,5 +1,8 @@
 import socket
 import threading
+from config import *
+from utils import *
+
 
 class Tunnel:
     def __init__(self, first_socket: socket.socket, second_socket: socket.socket):
@@ -26,7 +29,49 @@ class Tunnel:
         while True:
             try:
                 message = self.second_socket.recv(1024).decode('ascii')
-                self.first_socket.send(message.encode('ascii'))
+                if message == UDP_NEEDED_MESSAGE:
+                    second_udp_socket = handle_udp_connection_request(self.second_socket)
+                    first_udp_socket, first_udp_port = udp_connection_request(self.first_socket)
+                    udp_tunnel = UDPTunnel(first_udp_socket, first_udp_port, second_udp_socket)
+                    udp_tunnel.run()
+                else:
+                    print(type(self.first_socket), self.first_socket)
+
+                    self.first_socket.send(message.encode('ascii'))
             except Exception as e:
                 print('Error Happened in Tunnel:', e)
                 break
+
+
+class UDPTunnel:
+    def __init__(self, first_socket: socket.socket, first_port: int, second_socket: socket.socket):
+        self.first_socket = first_socket
+        self.second_socket = second_socket
+        self.first_port = first_port
+
+    def run(self):
+        second_to_first_thread = threading.Thread(target=self.second_to_first)
+        second_to_first_thread.start()
+
+    def second_to_first(self):
+        while True:
+            try:
+                message = self.second_socket.recvfrom(BUFFER_SIZE)[0]
+                self.first_socket.sendto(message, (localhost, self.first_port))
+            except Exception as e:
+                print('Error Happened in Tunnel:', e)
+                break
+
+
+def connect_and_create_tunnel(client: socket.socket, dest_port: int, message_to_dest: str = None) -> Tunnel:
+    socket_to_dest_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print(dest_port)
+    socket_to_dest_server.connect((localhost, dest_port))
+
+    if message_to_dest is not None:
+        socket_to_dest_server.send(message_to_dest.encode('ascii'))
+
+    sleep(0.5)
+    t = Tunnel(first_socket=client, second_socket=socket_to_dest_server)
+    t.run()
+    return t
