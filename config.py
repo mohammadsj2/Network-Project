6842b375aebe901f44ebdde67374d9567ea9c71e
mohammadsj2 <mohammad.sj2@gmail.com>
2022-02-04 05:28:44 +0330

    Add config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+localhost = '127.0.0.1'
+SERVER_PORT = 8008
+CHATROOM_PORT = 8009
+STREAMER_PORT = 8010
+INVALID_PORTS = [SERVER_PORT, CHATROOM_PORT, STREAMER_PORT]
+server_to_port = {
+    'choghondar': CHATROOM_PORT,
+    'shalgham': STREAMER_PORT
+}
+VIDEO_NAMES = ['Pat & Mat Song', 'Zangoole', 'The Sheep', 'Tom & Jerry']
+VIDEO_PATHS = [
+    'videos/Pat_Mat_Theme.mp4',
+    'videos/Pexels1.mp4',
+    'videos/Pexels2.mp4',
+    'videos/Tom_and_Jerry.mp4',
+]
